b633faecd7a3edc4292e9439fba9637037b43dc6
sakhezech <contact@sakhezech.ru>
2023-09-26 01:20:38 +0300

    added default packaging logic

---
+++ src/cardscraper/package.py
@@ -0,0 +1,16 @@
+import os
+
+from genanki import Deck, Package
+
+
+def package(package_config, deck: Deck) -> None:
+    name = package_config['name'].removesuffix('.apkg')
+    out_path = package_config['output_path']
+    media = [
+        os.path.join(p, n)
+        for p, _, ns in os.walk(package_config['media'])
+        for n in ns
+    ]
+    package = Package(deck, media)
+    os.makedirs(out_path, exist_ok=True)
+    package.write_to_file(os.path.join(out_path, name + '.apkg'))
