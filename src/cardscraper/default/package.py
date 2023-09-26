0bf7935119f0f76b45ced380dde682300a14e5e4
sakhezech <contact@sakhezech.ru>
2023-09-26 22:44:01 +0300

    made media folder not required

--- src/cardscraper/default/package.py
+++ src/cardscraper/default/package.py
@@ -6,11 +6,14 @@ from genanki import Deck, Package
 def default_package(package_config, deck: Deck) -> None:
     name = package_config['name'].removesuffix('.apkg')
     out_path = package_config['output_path']
-    media = [
-        os.path.join(p, n)
-        for p, _, ns in os.walk(package_config['media'])
-        for n in ns
-    ]
+    if package_config.setdefault('media', None):
+        media = [
+            os.path.join(p, n)
+            for p, _, ns in os.walk(package_config['media'])
+            for n in ns
+        ]
+    else:
+        media = []
     package = Package(deck, media)
     os.makedirs(out_path, exist_ok=True)
     package.write_to_file(os.path.join(out_path, name + '.apkg'))
