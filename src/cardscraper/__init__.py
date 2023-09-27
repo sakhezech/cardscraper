dc7fb844e3c9e6b7874d9f0820f46deb225e3fd8
sakhezech <contact@sakhezech.ru>
2023-09-27 05:55:06 +0300

    changed isort profile to blue (black + line_length)

--- src/cardscraper/__init__.py
+++ src/cardscraper/__init__.py
@@ -1,2 +1,6 @@
-from cardscraper.process_file import (process_conf, process_conf_manual,
-                                      process_file, read_yaml)
+from cardscraper.process_file import (
+    process_conf,
+    process_conf_manual,
+    process_file,
+    read_yaml,
+)
--- src/cardscraper/default/__init__.py
+++ src/cardscraper/default/__init__.py
@@ -1,4 +1,4 @@
 from cardscraper.default.deck import default_deck
 from cardscraper.default.model import default_model
-from cardscraper.default.scraping import default_notes
 from cardscraper.default.package import default_package
+from cardscraper.default.scraping import default_notes
