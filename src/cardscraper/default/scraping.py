7356f35df07520c403ec3452a4f34358d712b545
sakhezech <contact@sakhezech.ru>
2024-01-03 23:54:29 +0300

    scraping long needed comments

--- src/cardscraper/default/scraping.py
+++ src/cardscraper/default/scraping.py
@@ -33,17 +33,48 @@ def generate_notes_for_quote(
     notes: list[Note] | None = None,
     info: dict | None = None,
 ) -> list[Note]:
+    """
+    Generated notes from the query tree.
+
+    Args:
+        tag (Tag):...
+        query (Query): Query tree starting node.
+        model (Model): genanki Model for the notes.
+        notes (list[Note]): List of notes to return. Leave None.
+        info (dict): Dictionary of collected results. Leave None.
+
+    Returns:
+        list[Note]: Generated notes.
+    """
     if notes is None:
         notes = []
     if info is None:
         info = {}
 
+    # this is a simplified way of how this function works
+    # info = {}
+    # for a in range(10):
+    #     info['a'] = a
+    #     for b in range(10):
+    #         info['b'] = b
+    #         for c in range(10):
+    #             info['c'] = c
+    #             make_note_from_info(info, ...)
+
+    # info is a dictionary of query names to their values that will be used
+    # to construct the field list for genanki.Note(...) in make_note_from_info.
+    # it is a mutable shared state that is passed in further on each recursive
+    # call where we store all evaluated query results.
+
+    # there are two types of queries: 'many' and non 'many' queries
+    # the difference is we have to loop over the 'many' queries
     selected_tags = tag.select(query.query)
     # non 'many' queries only get the first selected tag
     if not query.many and selected_tags:
         selected_tags = selected_tags[:1]
 
     for selected_tag in selected_tags:
+        # here we regex the result if we need to
         if query.regex is not None:
             text = re.search(query.regex, selected_tag.text, re.DOTALL)
             if text is None:
@@ -54,6 +85,11 @@ def generate_notes_for_quote(
         else:
             info[query.name] = selected_tag.text
 
+        # we separate the two types of queries to ensure that 'many' ones are
+        # processed last.
+        # we need to do that because we need to have all the information
+        # that will not be changed before we 'go down a level' and start
+        # evaluating the results there.
         normal_queries = [q for q in query.children if not q.many]
         many_queries = [q for q in query.children if q.many]
 
@@ -61,6 +97,8 @@ def generate_notes_for_quote(
         for child in normal_queries + many_queries:
             generate_notes_for_quote(selected_tag, child, model, notes, info)
 
+        # if we are the last 'many' query that means we are at the end of the
+        # loop and we need to make the note.
         if not many_queries and query.many:
             notes.append(make_note_from_info(info, model))
 
@@ -68,6 +106,16 @@ def generate_notes_for_quote(
 
 
 def make_note_from_info(info: dict, model: Model) -> Note:
+    """
+    Creates a Note from the collected info.
+
+    Args:
+        info (dict): Collected info from queries.
+        model (Model): genanki Model for the notes.
+
+    Returns:
+        Note: Constructed note.
+    """
     model_field_names = [
         field_name for fd in model.fields for _, field_name in fd.items()
     ]
@@ -76,6 +124,19 @@ def make_note_from_info(info: dict, model: Model) -> Note:
 
 
 def validate_query_tree(query: Query) -> bool:
+    """
+    Validates that the query tree is correct.
+
+    If there are multiple queries that we should loop over and they are not
+    under each other (i.e. each 'many' query is not the sole 'many' child of a
+    parent 'many' query) we have no idea how to loop over them.
+
+    Args:
+        query (Query): Query tree starting node.
+
+    Returns:
+        bool: Whether query tree is correct or not.
+    """
     queries_with_many_under = [
         validate_query_tree(child) for child in query.children
     ].count(True)
